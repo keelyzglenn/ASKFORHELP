a164b3612d866f77c64ee7b06e5964becf903f1f
keely glenn and sara frandsen <keely+sara@>
2017-02-09 11:13:18 -0800

    add all necessary pages and home modular paths to display game title list and each game title id page

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,38 @@
+using Nancy;
+using Games.Objects;
+using System;
+using System.Collections.Generic;
+
+namespace Games
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+
+// takes you to the add game page
+      Get["/games/add"] = _ => {
+        return View["games_add.cshtml"];
+      };
+// shows all games by title
+      Get["/all_games_by_title"] = _ => {
+        List<Game> allGames = Game.GetAll();
+        return View["view_all_games_by_title.cshtml", allGames];
+      };
+// uses the form to post each new game input
+      Post["/all_games_by_title"] = _ => {
+        Game newGame = new Game(Request.Form["new-game"]);
+        List<Game> allGames = Game.GetAll();
+        return View["view_all_games_by_title.cshtml", allGames];
+      };
+  // displays singular game title based on game title id
+      Get["/game/{id}"] = parameters => {
+        Game game = Game.Find(parameters.id);
+        return View["game_title.cshtml", game];
+      };
+    }
+  }
+}
